33447fb17dddd2291e44f78888f9ee2c64397a64
fabruizb <166636528+fabruizb@users.noreply.github.com>
2024-07-19 23:54:07 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,58 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generarCarta();
+  cuentaRegresiva();
+  document
+    .getElementById("generate-btn")
+    .addEventListener("click", generarCarta);
 };
+
+function generarCarta() {
+  const symbol = ["♦", "♥", "♠", "♣"];
+  const number = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const randomNumber = number[Math.floor(Math.random() * number.length)];
+  const randomSymbol = symbol[Math.floor(Math.random() * symbol.length)];
+
+  document.getElementById("number").innerHTML = randomNumber;
+  document.getElementById("top-symbol").innerHTML = randomSymbol;
+  document.getElementById("bottom-symbol").innerHTML = randomSymbol;
+
+  const isRedSuit = randomSymbol === "♥" || randomSymbol === "♦";
+  document.getElementById("top-symbol").style.color = isRedSuit
+    ? "red"
+    : "black";
+  document.getElementById("bottom-symbol").style.color = isRedSuit
+    ? "red"
+    : "black";
+}
+
+function cuentaRegresiva() {
+  let countdown = 10;
+  const countdownElement = document.getElementById("countdown");
+
+  setInterval(() => {
+    countdownElement.innerHTML = countdown;
+    if (countdown === 0) {
+      countdown = 10;
+      generarCarta();
+    } else {
+      countdown--;
+    }
+  }, 1000);
+}
